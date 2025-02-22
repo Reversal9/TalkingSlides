1d885ad5198822e575e7378c657f494ffbb64dd9
Jonnen Chong <jonnenc@gmail.com>
2025-02-22 12:26:57 -0500

    add url and views for video

--- backend/app/myapp/urls.py
+++ backend/app/myapp/urls.py
@@ -1,14 +1,17 @@
 from django.contrib import admin
 from django.urls import path
-from .views import get_message, upload_pdf, view_pdf, delete_pdf, index
+from .views import get_message, upload_pdf, view_pdf, delete_pdf, index, upload_video, view_video, delete_video
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('message/', get_message, name='get_message'),
-    path('upload/', upload_pdf, name='upload_pdf'),
-    path('view/<int:pdf_id>/', view_pdf, name='view_pdf'),
-    path('delete/<int:pdf_id>/', delete_pdf, name='delete_pdf'),
+    path('upload-pdf/', upload_pdf, name='upload_pdf'),
+    path('view-pdf/<int:pdf_id>/', view_pdf, name='view_pdf'),
+    path('delete-pdf/<int:pdf_id>/', delete_pdf, name='delete_pdf'),
+    path('upload-video/', upload_video, name='upload_video'),
+    path('view-video/<int:video_id>/', view_video, name='view_video'),
+    path('delete-video/<int:video_id>/', delete_video, name='delete_video'),
     path('message/', views.get_message, name='get_message'),
     path("login", views.login, name="login"),
     path("logout", views.logout, name="logout"),
