c0d2008d1e9cb453d2134818b0e7aff70873a426
Jonnen Chong <jonnenc@gmail.com>
2025-02-22 12:12:38 -0500

    add form"

--- backend/app/myapp/forms.py
+++ backend/app/myapp/forms.py
@@ -1,8 +1,13 @@
 # forms.py
 from django import forms
-from .models import Pdf
+from .models import Pdf, Video
 
 class PdfUploadForm(forms.ModelForm):
     class Meta:
         model = Pdf
+        fields = ['file']
+
+class VideoUploadForm(forms.ModelForm):
+    class Meta:
+        model = Video
         fields = ['file']
