4b80765600f6f9094cebfd9cf1290d767e47d94b
aadyar882 <rangoleaa@gmail.com>
2025-02-23 04:10:23 -0500

    adding dialogue functions

--- backend/app/myapp/ai/generate_audio.py
+++ backend/app/myapp/ai/generate_audio.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from openai import OpenAI
 import os
+import generate_text
 
 def add_voice(input_script):
     '''
@@ -24,5 +25,38 @@ def add_voice(input_script):
 
     return None
 
+def add_voice_dialogue(script_text, voice_opt1, voice_opt2):
+    client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
+    speaker_one_scripts, speaker_two_scripts = generate_text.extract_speaker_texts(script_text)
+    speaker_one_responses = []
+    speaker_two_responses = []
+
+    filenum = 0
+    for text in speaker_one_scripts: 
+      response = client.audio.speech.create(
+          model="tts-1",
+          voice=voice_opt1,
+          input=text,
+      ) 
+      # response.stream_to_file(f"speaker_1_{filenum}.mp3")
+      filenum += 1
+      if not (response and response.content):
+        return None
+      speaker_one_responses.append(response.content)
+
+    filenum = 0
+    for text in speaker_two_scripts: 
+      response = client.audio.speech.create(
+          model="tts-1",
+          voice=voice_opt2,
+          input=text,
+      ) 
+      # response.stream_to_file(f"speaker_2_{filenum}.mp3")
+      filenum += 1
+      if not (response and response.content):
+        return None
+      speaker_two_responses.append(response.content)
+
+    return speaker_one_responses, speaker_two_responses
 
